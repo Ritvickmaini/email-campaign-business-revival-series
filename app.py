5e225ddb1cc3ac9dda04b5f716d188ddedb5022d
Ritvick Maini <101634547+Ritvickmaini@users.noreply.github.com>
2026-01-15 15:52:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -77,7 +77,6 @@ def generate_email_html(
                   Regards<br/>
                   Customer Success Team<br/>
                   Business Revival Series<br/>
-                  (+44) 2034517166<br/>
                   3–4 March 2026 | London Olympia<br/>
                   </br>
                   On Behalf of<br/>
